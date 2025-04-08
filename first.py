42ffe353bc2d0d7dcb4661a02af4d6fc730831c8
leonischu <leonischu10@gmail.com>
2025-04-08 20:06:17 +0545

    series

--- first.py
+++ first.py
@@ -4,3 +4,18 @@ var=pd.Series(x,index=['a','s','d','m'],dtype="float" ,name='python')
 print(var)
 print(type(var))
 print(var[2])
+
+# using dict
+dic ={"name":['python','c','c++','java'],"por":[12,13,14,15],"rank":[1,2,3,4]}
+var1=pd.Series(dic)
+print(var1)
+
+#single data
+s=pd.Series(12,index=[1,2,3,4,5,6,7])
+print(s)
+print(type(s))
+
+#No Broadcast error
+s1=pd.Series(12,index=[1,2,3,4,5,6,7])
+s2=pd.Series(12,index=[1,2,3,4])
+print(s1+s2)
